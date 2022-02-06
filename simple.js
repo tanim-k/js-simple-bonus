2e14cc628cc90703c2eca0fe4e69c5bf6b6d7c72
tanim-k <tanimkhan877@gmail.com>
2022-02-06 10:15:33 +0600

    for loop

--- simple.js
+++ simple.js
@@ -1,3 +1,12 @@
-for(i = 1; i < 6; i++) {
+for(i = 1; i < 4; i++) {
     console.log(i);
 }
+// onnek vabe kora jai //
+function consoleNumber(i) {
+    if(i>3) {
+        return;
+    }
+    console.log(i);
+    consoleNumber(i+1);
+}
+consoleNumber(1);
---
+++ sum.js
@@ -0,0 +1,5 @@
+let sum = 0 ;
+for (i=3; i>=1; i--) {
+    sum = sum + i;
+}
+console.log(sum)
