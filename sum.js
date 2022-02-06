259ea2f4e230ccc56fa86bfd47f13b32d621b59f
tanim-k <tanimkhan877@gmail.com>
2022-02-06 10:32:57 +0600

    function for sum

--- sum.js
+++ sum.js
@@ -13,3 +13,12 @@ function sum(i) {
     return i + sum(--i);
 }
 console.log(sum(5));
+
+/* 
+i + sum(--i);
+5 + sum(4)
+5 + 4 + sum(3)
+5 + 4 + 3 + sum(2)
+5 + 4 + 3 + 2 + sum(1)
+5 + 4 + 3 + 2 + 1 + sum(0)
+*/
